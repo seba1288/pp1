beab86a070c67c38bd21e726b4cab3d3a6dfd960
Sebastian Wróbel <80533347+seba1288@users.noreply.github.com>
2023-10-10 21:16:44 +0200

    Add files via upload

---
+++ 02-TypesAndVariables/exercisees_python/Exercise3.py
@@ -0,0 +1,5 @@
+
+rate = float(input('enter rate: '))
+hours = int(input('enter hours: '))
+pay = rate * hours
+print("Pay: ",round(pay))
---
+++ 02-TypesAndVariables/exercisees_python/Exercise5.py
@@ -0,0 +1,6 @@
+
+
+
+temp_c = int(input("enter temperature: "))
+Fahrenheit = round(temp_c * (9/5)+ 32)
+print('temperature in Fahrenheit: ', Fahrenheit)
